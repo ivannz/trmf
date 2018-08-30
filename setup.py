f7756869bbc3b0f03bf3e0341b2557962f8701d3
ivannz <ivan.nazarov@skolkovotech.ru>
2018-08-30 17:13:03 +0300

    Added setup.py for simple installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+
+setup(
+    name="trmf",
+    version="0.8",
+    description="""A pythonic library of matrix decomposition methods """
+                """for multivariate time-series.""",
+    license="MIT License",
+    author="Ivan Nazarov",
+    author_email="ivan.nazarov@skolkovotech.ru",
+    packages=["trmf"],
+    requires=["numpy", "numba", "scipy"]
+)
