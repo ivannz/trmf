c9392373530ece3ef2862801758e620fd3c0bd00
ivannz <ivan.nazarov@skolkovotech.ru>
2018-11-27 12:34:20 +0300

    ENH: sparse matrix support in the optimization steps

---
+++ trmf/extmath.py
@@ -0,0 +1,45 @@
+# updated version of `trmf_v0.8.2-sparse.ipynb` as of 2018-11-26 12:00
+
+import numpy as np
+import numba as nb
+
+from sklearn.utils.extmath import safe_sparse_dot
+
+
+# In[26] -- sparse:
+@nb.njit("(float64, float64[:,::1], float64[:,::1], "
+         "float64, int32[::1], int32[::1], float64[::1])",
+         fastmath=True, error_model="numpy", parallel=True, cache=False)
+def _csr_gemm(alpha, X, D, beta, Sp, Sj, Sx):
+    # computes\mathcal{P}_\Omega(X D) -- n1 x n2 sparse matrix
+    if abs(beta) > 0:
+        for i in nb.prange(len(X)):
+            # compute e_i' XD e_{Sj[j]}
+            for j in range(Sp[i], Sp[i+1]):
+                dot = np.dot(X[i], D[:, Sj[j]])
+                Sx[j] = beta * Sx[j] + alpha * dot
+        # end for
+    else:
+        for i in nb.prange(len(X)):
+            # compute e_i' XD e_{Sj[j]}
+            for j in range(Sp[i], Sp[i+1]):
+                Sx[j] = alpha * np.dot(X[i], D[:, Sj[j]])
+        # end for
+    # end if
+
+
+def csr_gemm(alpha, X, D, beta, Y):
+    _csr_gemm(alpha, X, D, beta, Y.indptr, Y.indices, Y.data)
+#     dot = alpha * np.dot(X, D)[Y.nonzero()]
+#     if abs(beta) > 0:
+#         Y.data = beta * Y.data + dot
+#     else:
+#         Y.data = dot
+    return Y
+
+
+def csr_column_means(X):
+    f_sums = np.bincount(X.indices, weights=X.data, minlength=X.shape[1])
+    n_nnz = np.maximum(np.bincount(X.indices, minlength=X.shape[1]), 1.)
+
+    return (f_sums / n_nnz)[np.newaxis]
--- trmf/steps.py
+++ trmf/steps.py
@@ -1,4 +1,4 @@
-# stripped version of `trmf_v0.8.2.ipynb` as of 2018-08-29 16:00
+# updated version of `trmf_v0.8.2-sparse.ipynb` as of 2018-11-26 12:00
 
 import numpy as np
 import numba as nb
@@ -9,6 +9,7 @@ from numpy.lib.stride_tricks import as_strided
 from sklearn.utils.extmath import safe_sparse_dot
 
 from .tron import tron
+from .extmath import csr_gemm
 
 
 # In[13]:
@@ -81,13 +82,28 @@ def graph_hess_vect(V, F, adj):
     return graph_grad(V, adj)
 
 
-# In[28]:
+# In[28] -- sparse:
+def l2_loss_valj(Y, Z, F):
+    if sp.issparse(Y):
+        R = csr_gemm(1, Z, F, -1, Y.copy())
+        return sp.linalg.norm(R, ord="fro") ** 2
+
+    return np.linalg.norm(Y - np.dot(Z, F), ord="fro") ** 2
+
+
+# In[31]:
 def f_step_tron_valj(f, Y, Z, C_F, eta_F, adj):
     """Compute current value the f-step loss."""
     (n_samples, n_targets), n_components = Y.shape, Z.shape[1]
-    F = f.reshape(n_components, n_targets)
 
-    objective = np.linalg.norm(Y - np.dot(Z, F), ord="fro") ** 2
+    F = f.reshape(n_components, n_targets)
+    objective = l2_loss_valj(Y, Z, F)
+
+    if sp.issparse(Y):
+        coef = C_F * Y.nnz / (n_components * n_targets)
+    else:
+        coef = C_F * n_samples / n_components
+
     if C_F > 0:
         if eta_F < 1:
             reg_f_l2 = np.linalg.norm(F, ord="fro") ** 2
@@ -95,52 +111,70 @@ def f_step_tron_valj(f, Y, Z, C_F, eta_F, adj):
             reg_f_l2, eta_F = 0., 1.
         # end if
 
-        if eta_F > 0:
+        if sp.issparse(adj) and (eta_F > 0):
             reg_f_graph = np.linalg.norm(graph_resid(F, adj), ord="fro") ** 2
         else:
             reg_f_graph, eta_F = 0., 0.
         # end if
 
         reg_f = reg_f_l2 * (1 - eta_F) + reg_f_graph * eta_F
-        objective += reg_f * (C_F * n_samples / n_components)
+        objective += reg_f * coef
     # end if
 
     return 0.5 * objective
 
 
-# In[30]:
+# In[33]:
 def f_step_tron_grad(f, Y, Z, C_F, eta_F, adj):
     """Compute the gradient of the f-step objective."""
     (n_samples, n_targets), n_components = Y.shape, Z.shape[1]
-    coef = C_F * n_samples / n_components
 
     F = f.reshape(n_components, n_targets)
+    if sp.issparse(Y):
+        coef = C_F * Y.nnz / (n_components * n_targets)
 
-    ZTY, ZTZ = np.dot(Z.T, Y), np.dot(Z.T, Z)
-    if C_F > 0 and eta_F < 1:
-        ZTZ.flat[::n_components + 1] += (1 - eta_F) * coef
+        grad = safe_sparse_dot(Z.T, csr_gemm(1, Z, F, -1, Y.copy()))
+        grad += (1 - eta_F) * coef * F
 
-    grad = np.dot(ZTZ, F) - ZTY
-    if C_F > 0 and eta_F > 0:
+    else:
+        coef = C_F * n_samples / n_components
+
+        ZTY, ZTZ = np.dot(Z.T, Y), np.dot(Z.T, Z)
+        if C_F > 0 and eta_F < 1:
+            ZTZ.flat[::n_components + 1] += (1 - eta_F) * coef
+
+        grad = np.dot(ZTZ, F) - ZTY
+    # end if
+
+    if C_F > 0 and sp.issparse(adj) and eta_F > 0:
         grad += graph_grad(F, adj) * (eta_F * coef)
 
     return grad.reshape(-1)
 
 
-# In[31]:
+# In[34]:
 def f_step_tron_hess(v, Y, Z, C_F, eta_F, adj):
     """Get the Hessian-vector product for the f-step objective."""
     (n_samples, n_targets), n_components = Y.shape, Z.shape[1]
-    coef = C_F * n_samples / n_components
 
     V = v.reshape(n_components, n_targets)
+    if sp.issparse(Y):
+        coef = C_F * Y.nnz / (n_components * n_targets)
 
-    ZTZ = np.dot(Z.T, Z)
-    if C_F > 0 and eta_F < 1:
-        ZTZ.flat[::n_components + 1] += (1 - eta_F) * coef
+        hess_v = safe_sparse_dot(Z.T, csr_gemm(1, Z, V, 0, Y.copy()))
+        hess_v += (1 - eta_F) * coef * V
 
-    hess_v = np.dot(ZTZ, V)
-    if C_F > 0 and eta_F > 0:
+    else:
+        coef = C_F * n_samples / n_components
+
+        ZTZ = np.dot(Z.T, Z)
+        if C_F > 0 and eta_F < 1:
+            ZTZ.flat[::n_components + 1] += (1 - eta_F) * coef
+
+        hess_v = np.dot(ZTZ, V)
+    # end if
+
+    if C_F > 0 and sp.issparse(adj) and eta_F > 0:
         hess_v += graph_grad(V, adj) * (eta_F * coef)
 
     return hess_v.reshape(-1)
@@ -230,7 +264,7 @@ def f_step(F, Y, Z, C_F, eta_F, adj, kind="fgm", **kwargs):
     return F, lip
 
 
-# In[38]:
+# In[41]:
 def phi_step(phi, Z, C_Z, C_phi, eta_Z, nugget=1e-8):
     """Compute the Ridge regression estimate of the AR(p) coefficients.
 
@@ -273,14 +307,20 @@ def phi_step(phi, Z, C_Z, C_phi, eta_Z, nugget=1e-8):
     return np.einsum("ijk,ij,isj,is->ik", Vh, gain, U, y)
 
 
-# In[43]:
+# In[46]:
 def z_step_tron_valh(z, Y, F, phi, C_Z, eta_Z):
     """Get the value of the z-step objective."""
     n_samples, n_targets = Y.shape
     n_components, n_order = phi.shape
-    Z = z.reshape(n_samples, n_components)
 
-    objective = np.linalg.norm(Y - np.dot(Z, F), ord="fro") ** 2
+    Z = z.reshape(n_samples, n_components)
+    objective = l2_loss_valj(Y, Z, F)
+
+    if sp.issparse(Y):
+        coef = C_Z * Y.nnz / (n_samples * n_components)
+    else:
+        coef = C_Z * n_targets / n_components
+
     if C_Z > 0:
         if eta_Z < 1:
             reg_z_l2 = np.linalg.norm(Z, ord="fro") ** 2
@@ -295,28 +335,37 @@ def z_step_tron_valh(z, Y, F, phi, C_Z, eta_Z):
             reg_z_ar, eta_Z = 0., 0.
         # end if
 
-        # reg_z was implicitly scaled by T d
+        # reg_z was implicitly scaled by T d or nnz(Y)
         reg_z = reg_z_l2 * (1 - eta_Z) + reg_z_ar * eta_Z
-        objective += reg_z * (C_Z * n_targets / n_components)
+        objective += reg_z * coef
     # end if
 
     return 0.5 * objective
 
 
-# In[45]:
+# In[48]:
 def z_step_tron_grad(z, Y, F, phi, C_Z, eta_Z):
     """Compute the gradient of the z-step objective."""
     n_samples, n_targets = Y.shape
     n_components, n_order = phi.shape
-    coef = C_Z * n_targets / n_components
 
     Z = z.reshape(n_samples, n_components)
+    if sp.issparse(Y):
+        coef = C_Z * Y.nnz / (n_samples * n_components)
 
-    YFT, FFT = np.dot(Y, F.T), np.dot(F, F.T)
-    if C_Z > 0 and eta_Z < 1:
-        FFT.flat[::n_components + 1] += (1 - eta_Z) * coef
+        grad = safe_sparse_dot(csr_gemm(1, Z, F, -1, Y.copy()), F.T)
+        grad += (1 - eta_Z) * coef * Z
 
-    grad = np.dot(Z, FFT) - YFT
+    else:
+        coef = C_Z * n_targets / n_components
+
+        YFT, FFT = np.dot(Y, F.T), np.dot(F, F.T)
+        if C_Z > 0 and eta_Z < 1:
+            FFT.flat[::n_components + 1] += (1 - eta_Z) * coef
+
+        grad = np.dot(Z, FFT) - YFT
+    # end if
+
     if C_Z > 0 and eta_Z > 0:
         ratio = n_samples / (n_samples - n_order)
         grad += ar_grad(Z, phi) * (ratio * eta_Z * coef)
@@ -324,28 +373,38 @@ def z_step_tron_grad(z, Y, F, phi, C_Z, eta_Z):
     return grad.reshape(-1)
 
 
-# In[46]:
+# In[49]:
 def z_step_tron_hess(v, Y, F, phi, C_Z, eta_Z):
     """Compute the Hessian-vector product of the z-step objective for v."""
     n_samples, n_targets = Y.shape
     n_components, n_order = phi.shape
-    coef = C_Z * n_targets / n_components
 
     V = v.reshape(n_samples, n_components)
+    if sp.issparse(Y):
+        coef = C_Z * Y.nnz / (n_samples * n_components)
 
-    FFT = np.dot(F, F.T)
-    if C_Z > 0 and eta_Z < 1:
-        FFT.flat[::n_components + 1] += (1 - eta_Z) * coef
+        hess_v = safe_sparse_dot(csr_gemm(1, V, F, 0, Y.copy()), F.T)
+        hess_v += (1 - eta_Z) * coef * V
 
-    hess_v = np.dot(V, FFT)
+    else:
+        coef = C_Z * n_targets / n_components
+
+        FFT = np.dot(F, F.T)
+        if C_Z > 0 and eta_Z < 1:
+            FFT.flat[::n_components + 1] += (1 - eta_Z) * coef
+
+        hess_v = np.dot(V, FFT)
+    # end if
+
     if C_Z > 0 and eta_Z > 0:
+        # should call ar_hess_vect(V, Z, adj) but no Z is available
         ratio = n_samples / (n_samples - n_order)
-        hess_v += ar_grad(V, phi) * (ratio * eta_Z * coef)
+        hess_v += ar_grad(V, phi) * ratio * eta_Z * coef
 
     return hess_v.reshape(-1)
 
 
-# In[47]:
+# In[50]:
 def z_step_tron(Z, Y, F, phi, C_Z, eta_Z, rtol=5e-2, atol=1e-4, verbose=False):
     """TRON solver for the f-step minimization problem."""
     f_call = z_step_tron_valh, z_step_tron_grad, z_step_tron_hess
